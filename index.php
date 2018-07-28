bc89b1c71f42a22b761f098062af43797c4417f2
kiiaroto <kiiaroto@gmail.com>
2018-07-28 13:42:35 +0200

    Changed some typing mistakes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require('require-ressources/header/navbar.php');
     <div id="home-main" class="container">
         <div class="jumbotron">
             <h1>Dylan OMAN</h1>
-            <p class="text-left">Bienvenue sur mon portefolio.<br>Vous y trouverez certain projet sur lesquelle j'ai travailler, ainsi que mon CV.</p>
+            <p class="text-left">Bienvenue sur mon portefolio.<br>Vous y trouverez certains projets sur lesquels j'ai travaillé, ainsi que mon CV.</p>
         </div>
     </div>
 </div>
