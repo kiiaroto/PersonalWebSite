316a25719e741002cb81400fa578f0de1cbbe9be
kiiaroto <kiiaroto@gmail.com>
2018-09-29 22:06:07 +0200

    Deleted parallax.js cause it's not used anymore

--- assets/js/parallax.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){    
-    $('#landing-content').mousemove(function(e){
-    var x = -(e.pageX + this.offsetLeft) / 20;
-    var y = -(e.pageY + this.offsetTop) / 20;
-    $(this).css('background-position', (-125 + x) + 'px ' + (-105 + y) + 'px');
-  });    
-});
